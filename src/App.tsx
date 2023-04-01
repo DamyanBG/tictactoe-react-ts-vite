a131dcb3adf22d7658f8cc4e5acfec27481bd140
DamyanBG <d.dimitrov.1988@abv.bg>
2023-04-01 22:15:55 +0300

    format app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import './App.css'
 import MainPage from './components/MainPage'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
 
+function App() {
   return (
     <div className="App">
       <main>
